72ae35397497a502db7565ea62a83392f2840831
Christian <duoduchristian36@gmail.com>
2023-01-20 10:18:48 +0000

    main commit
    this commit contains all files relating to the school dashboard

---
+++ js/main.js
@@ -0,0 +1,111 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Sidebar Toggler
+    $('.sidebar-toggler').click(function () {
+        $('.sidebar, .content').toggleClass("open");
+        return false;
+    });
+
+
+    
+
+
+   
+
+    // Staff members carousel
+    $(".staff").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : false
+    });
+
+
+    // Students
+    var ctx1 = $("#students").get(0).getContext("2d");
+    var myChart1 = new Chart(ctx1, {
+        type: "bar",
+        data: {
+            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            datasets: [{
+                    label: "Boys",
+                    data: [15, 30, 55, 65, 60, 80, 95],
+                    backgroundColor: "black"
+                },
+                {
+                    label: "Girls",
+                    data: [8, 35, 40, 60, 70, 55, 75],
+                    backgroundColor: "rgba(0, 156, 255, .5)"
+                },
+                
+            ]
+            },
+        options: {
+            responsive: true
+        }
+    });
+
+
+    // Overview
+    var ctx2 = $("#overview").get(0).getContext("2d");
+    var myChart2 = new Chart(ctx2, {
+        type: "line",
+        data: {
+            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            datasets: [{
+                    label: "Teachers",
+                    data: [15, 30, 55, 45, 70, 65, 85],
+                    backgroundColor: "green",
+                    fill: true
+                },
+                {
+                    label: "Students",
+                    data: [99, 135, 170, 130, 190, 180, 270],
+                    backgroundColor: "rgba(0, 156, 255, .3)",
+                    fill: true
+                }
+            ]
+            },
+        options: {
+            responsive: true
+        }
+    });
+    
+
+
+   
+
+
+    
+
+    
+})(jQuery);
+
